c2232560c1b5348e466cea95bafd78d381ec1364
Ryan Ho <ryho@cisco.com>
2024-07-20 17:54:17 -0700

    Updated sidebar header 'Policy'

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -345,7 +345,7 @@ const Sidebar = ({
 
           <div>
             <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
-              PRIVACY
+              POLICY
             </h3>
 
             <ul className="mb-6 flex flex-col gap-1.5">
